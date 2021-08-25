8d21c5257e5462b96f14a05908f313d197293a63
Tyler Dodge <tylerdodge@Tylers-MacBook-Pro-2.local>
2021-08-25 08:46:44 -0700

    Switch to correct revision for emacs 27.1

--- nix/emacs-27.1.nix
+++ nix/emacs-27.1.nix
@@ -3,7 +3,7 @@ let
     name = "emacs-revision-27.1";
     url = "https://github.com/NixOS/nixpkgs/";                       
     ref = "refs/heads/nixpkgs-unstable";                     
-    rev = "860b56be91fb874d48e23a950815969a7b832fbc";    
+    rev = "a765beccb52f30a30fee313fbae483693ffe200d";
   }) {};
   emacsWithPackages = import ./emacs-packages.nix {
     emacsWithPackages = with pkgs; (emacsPackagesNgGen emacs).emacsWithPackages;
